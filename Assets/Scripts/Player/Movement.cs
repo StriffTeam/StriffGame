003e3144fd3d58ee8df5cfa6fc2d19e47b5440ce
Artun Çolak <artuncolak97@mail.com>
2020-04-10 19:44:52 +0300

    Added Cooking & Trash Events

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -3,6 +3,8 @@
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2d;
+    private bool isNearCookingSpot;
+    private bool isNearTrash;
     public int speed = 10;
 
     private void Start()
@@ -13,6 +15,41 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
+    }
+
+    private void Update()
+    {
+        Action();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("CookingSpot"))
+            isNearCookingSpot = true;
+        else if (collider.CompareTag("Trash")) isNearTrash = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("CookingSpot") || collider.CompareTag("Trash"))
+        {
+            isNearTrash = false;
+            isNearCookingSpot = false;
+        }
+    }
+
+    private void Action()
+    {
+        if (isNearTrash)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                Debug.Log("Çöpe Atılıyor...");
+        }
+        else if (isNearCookingSpot)
+        {
+            if (Input.GetKey(KeyCode.Space))
+                Debug.Log("Pişiriliyor...");
+        }
     }
 
     private void MovePlayer()
