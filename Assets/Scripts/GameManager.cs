1e0dd2a1f3a3521a86f41d353d3c8cb2066e6891
Artun Çolak <artuncolak97@mail.com>
2020-04-12 15:05:33 +0300

    Changed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,14 +18,14 @@ public class GameManager : MonoBehaviour
 
     private void GenerateRandomIngredients()
     {
-        if (ingredientsCount <= 5)
+        if (ingredientsCount <= 20)
             for (var i = 0; i < fallingIngredients.Length; i++)
                 if (fallingIngredients[i] == null && Time.time > dropTime)
                 {
                     var position = new Vector2(Random.Range(-7.0f, 7.0f), startZoneTransform.position.y);
-                    fallingIngredients[i] = Instantiate(ingredients[Random.Range(0, 4)], position, Quaternion.identity);
+                    fallingIngredients[i] = Instantiate(ingredients[Random.Range(0, ingredients.Length)], position, Quaternion.identity);
                     ingredientsCount++;
-                    dropTime = Time.time + 0.5f;
+                    dropTime = Time.time + 0.1f;
                 }
     }
 }
