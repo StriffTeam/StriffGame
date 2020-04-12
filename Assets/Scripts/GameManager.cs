7dcfcfc4f9fda7047abd9b46847965012704174a
Sefa Ufuk Demir <sefa_ufuk@hotmail.com.tr>
2020-04-12 15:34:08 +0300

    Added Somethings

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,21 @@
 public class GameManager : MonoBehaviour
 {
     private float dropTime;
+    private float dropTimeKnife;
     private readonly GameObject[] fallingIngredients = new GameObject[4];
 
     [SerializeField] private GameObject[] ingredients;
+    [SerializeField] private GameObject Knife;
 
     public static int ingredientsCount;
+    public static int knifeCount;
 
     [SerializeField] private Transform startZoneTransform;
 
     private void Update()
     {
         GenerateRandomIngredients();
+        GenerateKnife();
     }
 
     private void GenerateRandomIngredients()
@@ -27,5 +31,21 @@ public class GameManager : MonoBehaviour
                     ingredientsCount++;
                     dropTime = Time.time + 0.1f;
                 }
+    }
+
+    private void GenerateKnife()
+    {
+        if (knifeCount <= 3)
+        {
+            if (Time.time > dropTimeKnife)
+            {
+                var position = new Vector2(Random.Range(-7.0f, 7.0f), startZoneTransform.position.y);
+                Instantiate(Knife, position, Quaternion.identity);
+                knifeCount++;
+                dropTimeKnife = Time.time + 0.8f;
+            }
+               
+            
+        }
     }
 }
